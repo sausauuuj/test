d3dfb9746a4622f9740237c84b7a7ddbb016e368
sausauuuj <sausauuuj@gmail.com>
2026-03-30 19:19:36 +0800

    Delete plan b directory

--- plan b/config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-return [
-    'name' => 'Asset Management System',
-    'timezone' => 'Asia/Manila',
-    'currency' => 'PHP',
-];
--- plan b/officers.php
+++
@@ -1,5 +0,0 @@
-<?php
-declare(strict_types=1);
-
-header('Location: index.php#manage', true, 302);
-exit;
